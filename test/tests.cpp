ef8755025f079dbc4a40dafbbbc3771f8ec6da26
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2023-02-28 12:21:54 +0100

    update
    - update conan to >= 1.54 to install eigen and catch2
    - conan >= 1.54 is required
    - use eigen 3.4
    - use catch 3.3.1
    - fix tests issues

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,4 @@
-#include <catch2/catch.hpp>
+#include <catch2/catch_all.hpp>
 
 unsigned int Factorial(unsigned int number)
 {
--- test/tests_eigen.cpp
+++ test/tests_eigen.cpp
@@ -1,6 +1,6 @@
 #include <Eigen/Core>
 
-#include <catch2/catch.hpp>
+#include <catch2/catch_all.hpp>
 
 
 
